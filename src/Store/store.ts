65d898afc72bce6fbd148e6ce54f8c018cef1d68
Oleg <9helgi9gmail.com>
2022-11-26 10:16:36 +0100

    refactored

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,14 +1,21 @@
-import {combineReducers, createStore} from "redux";
+import {combineReducers, compose, createStore} from "redux";
 import {tasksReducer} from "../Reducers/tasksReducer";
 import {todoListReducer} from "../Reducers/todoListReducer";
 
+declare global {
+    interface  Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof  compose;
+    }
+}
 
 const rootReducer = combineReducers({
     todoLists: todoListReducer,
     tasks: tasksReducer
 })
 
-export const store = createStore(rootReducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(rootReducer, composeEnhancers())
 
 export type AppRootState = ReturnType<typeof rootReducer>
 // @ts-ignore
